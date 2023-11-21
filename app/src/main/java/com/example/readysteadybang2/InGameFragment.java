d9b8fed16100853a3fc26b9f5e099d1ca5834b23
loic <loic.bazonnais@gmail.com>
2023-11-21 17:05:51 +0100

    Premiers bouton cliquable dans la page principale. Pour l'instant il envoit en dehors de l'application.

--- app/src/main/java/com/example/readysteadybang2/InGameFragment.java
+++ app/src/main/java/com/example/readysteadybang2/InGameFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class InGameFragment extends Fragment {
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
