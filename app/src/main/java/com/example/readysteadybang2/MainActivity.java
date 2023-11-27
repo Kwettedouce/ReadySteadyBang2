8ec6a8c8ef1f22cf2c8ce1ac8e95d455b09a8903
loic <loic.bazonnais@gmail.com>
2023-11-27 23:39:53 +0100

    Structure d'activity (Main, Acti2 3 et 4) (voir shéma Discord)

--- app/src/main/java/com/example/readysteadybang2/MainActivity.java
+++ app/src/main/java/com/example/readysteadybang2/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +22,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+            }
+        });
     }
 
     public void openActivity2(){
         Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, Activity4.class);
         startActivity(intent);
     }
 }
