ad0b2fad649af937477728be746ee8e927fae441
loic <loic.bazonnais@gmail.com>
2023-11-29 17:58:31 +0100

    apparition du Ready en PvP

--- app/src/main/java/com/example/readysteadybang2/MainActivity2.java
+++ app/src/main/java/com/example/readysteadybang2/MainActivity2.java
@@ -4,17 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private TextView textReady;
     public boolean isFirstActivityOpened = false;
     public boolean isSecondeActivityOpened = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textReady = findViewById(R.id.textReady);
+
+        long delayMillis = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Affichage du texte après le délai
+                textReady.setText("READY");
+            }
+        }, delayMillis);
     }
 
     public void onImage1Click(View view){
