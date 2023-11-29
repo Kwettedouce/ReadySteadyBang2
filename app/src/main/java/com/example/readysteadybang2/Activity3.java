79bf16a1896acd3a67308d5158f6f83fb6596f5d
loic <loic.bazonnais@gmail.com>
2023-11-29 21:54:49 +0100

    Clik pour le tire en PvE

--- app/src/main/java/com/example/readysteadybang2/Activity3.java
+++ app/src/main/java/com/example/readysteadybang2/Activity3.java
@@ -2,65 +2,67 @@ package com.example.readysteadybang2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class Activity3 extends AppCompatActivity {
 
-    private TextView textReady;
+    private TextView textReadybis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
 
-        textReady = findViewById(R.id.textReady);
+        textReadybis = findViewById(R.id.textReadybis);
 
-        long delayMillis = 1500;
-        long delay2Millis = 10;
-        int randomDelay = generateRandomDelay();
+        long delayMillisbis = 1500;
+        long delay2Millisbis = 10;
+        int randomDelaybis = generateRandomDelay();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // Affichage du texte après le délai
-                textReady.setText("READY");
+                textReadybis.setText("READY");
 
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         // Disparition du premier texte
-                        textReady.setText("");
+                        textReadybis.setText("");
 
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 // Affichage du deuxième texte
-                                textReady.setText("STEADY");
+                                textReadybis.setText("STEADY");
 
                                 new Handler().postDelayed(new Runnable() {
                                     @Override
                                     public void run() {
                                         // Affichage du deuxième texte
-                                        textReady.setText("");
+                                        textReadybis.setText("");
                                         new Handler().postDelayed(new Runnable() {
                                             @Override
                                             public void run() {
                                                 // Affichage du deuxième texte
-                                                textReady.setText("BANG !");
+                                                textReadybis.setText("BANG !");
                                             }
-                                        }, randomDelay);
+                                        }, randomDelaybis);
                                     }
-                                }, delayMillis);
+                                }, delayMillisbis);
                             }
-                        }, delayMillis);
+                        }, delayMillisbis);
                     }
-                }, delayMillis);
+                }, delayMillisbis);
             }
-        }, delayMillis);
+        }, delayMillisbis);
     }
 
     private int generateRandomDelay() {
@@ -69,5 +71,10 @@ public class Activity3 extends AppCompatActivity {
 
         // Générer un nombre aléatoire entre 0 et 5000
         return random.nextInt(5000);
+    }
+
+    public void onImageClickbis(View view) {
+        Intent intent = new Intent(this, Activity11.class);
+        startActivity(intent);
     }
 }
