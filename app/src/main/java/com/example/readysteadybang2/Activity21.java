3ffe7316e36267f2b7e0aa88654f8e32fef23f47
loic <loic.bazonnais@gmail.com>
2023-11-29 22:33:39 +0100

    Fonction rejouer PvP

--- app/src/main/java/com/example/readysteadybang2/Activity21.java
+++ app/src/main/java/com/example/readysteadybang2/Activity21.java
@@ -2,13 +2,32 @@ package com.example.readysteadybang2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity21 extends AppCompatActivity {
+
+    private Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_21);
+
+        buttonBack = (Button) findViewById(R.id.buttonBack);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
