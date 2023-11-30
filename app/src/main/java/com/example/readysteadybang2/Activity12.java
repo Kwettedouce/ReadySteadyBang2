0e59ca7313a7a6d6c5ebdf9a98e1a346334351bb
loic <loic.bazonnais@gmail.com>
2023-12-01 00:32:00 +0100

    Logique bouton sur Activity12

--- app/src/main/java/com/example/readysteadybang2/Activity12.java
+++ app/src/main/java/com/example/readysteadybang2/Activity12.java
@@ -2,13 +2,52 @@ package com.example.readysteadybang2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity12 extends AppCompatActivity {
+
+    private Button buttonBack;
+
+    private Button buttonMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_12);
+        setContentView(R.layout.activity_11);
+
+        buttonBack = (Button) findViewById(R.id.buttonBack);
+        buttonMenu = (Button) findViewById(R.id.buttonMenu);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenu();
+            }
+        });
+    }
+
+    public void openActivity3(){
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
+
+    public void openMenu(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
