13693327376239b2214d2d85bfc1639576b34055
loic <loic.bazonnais@gmail.com>
2023-11-30 02:41:51 +0100

    retour Menu depuis les regles du jeu

--- app/src/main/java/com/example/readysteadybang2/Activity4.java
+++ app/src/main/java/com/example/readysteadybang2/Activity4.java
@@ -2,18 +2,37 @@ package com.example.readysteadybang2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity4 extends AppCompatActivity {
+
+    private Button buttonMenu4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+
+        buttonMenu4 = (Button) findViewById(R.id.buttonMenu4);
+
+        buttonMenu4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenu4();
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         return;
+    }
+
+    public void openMenu4(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
