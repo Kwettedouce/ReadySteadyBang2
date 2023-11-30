fa3af5178906760258c431a22e9d4932e9bed176
loic <loic.bazonnais@gmail.com>
2023-11-30 16:05:31 +0100

    liens entre ready et game (pvp et pve)

--- app/src/main/java/com/example/readysteadybang2/ActivityReadyPve.java
+++ app/src/main/java/com/example/readysteadybang2/ActivityReadyPve.java
@@ -2,18 +2,37 @@ package com.example.readysteadybang2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityReadyPve extends AppCompatActivity {
+
+    private Button ReadyToPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ready_pve);
+
+        ReadyToPlay = (Button) findViewById(R.id.ReadyToPlay);
+
+        ReadyToPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         return;
+    }
+
+    public void openActivity3(){
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/readysteadybang2/ActivityReadyPvp.java
+++ app/src/main/java/com/example/readysteadybang2/ActivityReadyPvp.java
@@ -2,18 +2,37 @@ package com.example.readysteadybang2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityReadyPvp extends AppCompatActivity {
+
+    private Button ReadyToPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ready_pvp);
+
+        ReadyToPlay = (Button) findViewById(R.id.ReadyToPlay);
+
+        ReadyToPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         return;
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
